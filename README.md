76e10f16dc89eb8b2deb57c97198772c1345f86a
LiangchengR <liangchengr@gmail.com>
2021-09-29 10:55:30 -0700

    added functionality to update square values to X and O's. Added turn counter

--- README.md
+++ README.md
@@ -6,8 +6,14 @@ This project will challenge your knowledge of React but should also be an exerci
 
 ### User Stories:
 - As a user, I should start off seeing a 3x3 grid on the main page.
+    - used map on squares array and formatted squares and gameboard
+
 - As a user, I should be able to click on a square to mark it.
 - As a user, my partner should be able to click on a square after me and see their mark.
+    - added functions for handling game play based on onClick
+    - passed index, value, and handleGamePlay as props
+
+
 - As a user, I shouldn't be able to click on a square that has already been selected.
 - As a user, when someone has won the game (3 squares in a row: horizontally, vertically, or diagonally) I should see a notice telling me who won.
 - As a user, I should not be able to continue playing the game after the game has been won.
--- src/App.js
+++ src/App.js
@@ -6,19 +6,52 @@ class App extends Component{
   constructor(props){
     super(props)
     this.state = {
-      squares: [0, 0, 0, 0, 0, 0, 0, 0, 0]
+      squares: ["", "", "", "", "", "", "", "", ""],
+      turn: 1, //counter to keep track of turns
     }
+  }
+
+  handleGamePlay = (index) => {
+    //destructuring
+    const {squares, turn} = this.state
+
+    //if first turn (or odd number on counter)-> then return X
+    if (turn%2 !== 0){
+      squares[index] = "❎"
+      let newTurn = turn + 1
+      this.setState({squares: squares, turn: newTurn})
+    }
+
+    //if after (if even number counter) -> return O
+    else {
+      squares[index] = "🅾️"
+      let newTurn = turn + 1
+      this.setState({squares: squares, turn: newTurn})
+    }
+
   }
 
   render(){
     return(
       <>
         <h1>Tic Tac Toe</h1>
-        <div className="gameboard">{this.state.squares.map(value => {
-          return <Square />
-        })}</div>
+        <div className="gameboard">
+          {this.state.squares.map((value, index) => {
+            return (
+              <Square 
+                value = {value}
+                index = {index}
+                key = {index}
+                handleGamePlay = {this.handleGamePlay}
+
+
+              />
+            )
+          })}
+        </div>
       </>
     )
   }
 }
+
 export default App
